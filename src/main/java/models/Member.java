c8dfe7d314c1fa1ecdd27df7b3e1d279725a32e4
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-07-11 23:33:12 -0300

    first commit

---
+++ src/main/java/models/Member.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.Date;
+
+public class Member {
+    private int memberId;
+    private String name;
+    private String lastName;
+    private int nationalId;
+    private String email;
+    private boolean state;
+    private Date enrollmentDate;
+}
